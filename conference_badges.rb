c9a5cba8141ffd2af4a0ff737021bad2a732c21d
Christine Contreras <christineec23@yahoo.com>
2021-08-09 17:25:59 -0500

    working

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,37 @@
 # Write your code here.
+def badge_maker(name)
+    "Hello, my name is #{name}."
+end
+
+
+def batch_badge_creator(array)
+    # array_badges = []
+    # array.each do |name|
+    #     array_badges.push("Hello, my name is #{name}.")
+    # end
+    # array_badges
+    array.collect do |name|
+        "Hello, my name is #{name}."
+    end
+end
+
+
+def assign_rooms(array)
+    new_array = []
+    array.each.with_index(1) do |name, index|
+        new_array.push("Hello, #{name}! You'll be assigned to room #{index}!")
+    end
+
+    new_array
+end
+
+
+def printer(array)
+    batch_badge_creator(array).each do |badge|
+        puts badge
+    end
+
+    assign_rooms(array).each do |room|
+        puts room
+    end
+end 
